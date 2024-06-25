ec4edf07175fd64d58283d63bb9bf8e3476e9467
Luciano S. Santana <lucianosantanabr@gmail.com>
2024-06-25 10:44:40 -0300

    Update SubscriptionResolver.java

--- graphql-subscription/src/main/java/com/subscription/SubscriptionResolver.java
+++ graphql-subscription/src/main/java/com/subscription/SubscriptionResolver.java
@@ -1,1 +1,21 @@
-package com.subscription;import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;import com.subscription.model.StockPrice;import java.time.Duration;import java.time.LocalDateTime;import java.util.Random;import org.reactivestreams.Publisher;import org.springframework.stereotype.Component;import reactor.core.publisher.Flux;@Componentpublic class SubscriptionResolver implements GraphQLSubscriptionResolver {  public Publisher<StockPrice> stockPrice(String symbol) {    Random random = new Random();    return Flux.interval(Duration.ofSeconds(1))        .map(num -> new StockPrice(symbol, random.nextDouble(), LocalDateTime.now()));  }}
+package com.subscription;
+
+import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
+import com.subscription.model.StockPrice;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Random;
+import org.reactivestreams.Publisher;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
+public class SubscriptionResolver implements GraphQLSubscriptionResolver {
+
+  public Publisher<StockPrice> stockPrice(String symbol) {
+
+    Random random = new Random();
+    return Flux.interval(Duration.ofSeconds(1))
+        .map(num -> new StockPrice(symbol, random.nextDouble(), LocalDateTime.now()));
+  }
+}
